881327f6786f958189922370e9160883c7ced62c
Sachin Jangid <sachinreal2003@gmail.com>
2026-01-28 01:37:09 +0530

    allowed localhost for backend from cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+  
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
